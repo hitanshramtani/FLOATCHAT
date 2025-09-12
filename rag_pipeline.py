a9fcb9f98bdd107d9e262ce393160717eef5c0f7
hitanshramtani <hitanshramtani@gmail.com>
2025-09-12 23:49:01 +0530

    Version 1

---
+++ rag_pipeline.py
@@ -0,0 +1,50 @@
+import os
+import re
+import sqlite3
+import pandas as pd
+import pickle
+from typing import Tuple, List
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_openai import ChatOpenAI
+from langchain.schema import HumanMessage, SystemMessage
+from langchain.prompts import ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate
+from dotenv import load_dotenv
+load_dotenv()
+VECTORSTORE_PATH = "./faiss_index"
+SQLITE_PATH = "./argo_cleaned.db"
+TABLE_NAME = "profiles"
+embed = OpenAIEmbeddings(model="text-embedding-3-small")
+vectorstore = FAISS.load_local(VECTORSTORE_PATH, embed, allow_dangerous_deserialization=True)
+from SCHEMA import DB_SCHEMA, PROMPT
+db_schema = DB_SCHEMA.format(TABLE_NAME=TABLE_NAME)
+def retrieve_context(query: str, k: int = 6):
+    docs = vectorstore.similarity_search(query, k=k)
+    contexts = [d.page_content for d in docs]
+    return contexts, docs
+def get_sql_query(contexts: List[str], user_question: str) -> str:
+    system_message = SystemMessage(content="You are an expert translator from English queries to SQL for the ARGO profiles database. You are given the database schema and some context information from the database. Use this information to generate accurate SQL queries.")
+    human_template = PROMPT.format(table=TABLE_NAME, db_schema=db_schema, context="\n".join(contexts), question=user_question)
+    human_message = HumanMessage(content=human_template)
+    chatbot = ChatOpenAI(model_name="gpt-4o-mini",temperature=0.0)
+    result = chatbot.invoke([system_message, human_message])
+    sql_text = result.content.strip()
+    return sql_text
+def get_data_from_sql(sql_query):
+    conn = sqlite3.connect(SQLITE_PATH)
+    try:
+        df = pd.read_sql_query(sql_query, conn)
+    finally:
+        conn.close()
+    return df   
+def main(user_question = None):
+    if user_question:
+        user_question = user_question
+    else:
+        user_question = input("Enter the Question: ")
+    # user_question  = "Show me salinity and temprature profiles from floats near 80 east and 90 east in january 2025"
+    contexts, docs = retrieve_context(user_question, k=10)
+    sql_query = get_sql_query(contexts, user_question)
+    print("Generated SQL Query: ",sql_query)
+    df = get_data_from_sql(sql_query)
+    return df
---
+++ stream3.py
@@ -0,0 +1,109 @@
+# streamlit_app.py
+import streamlit as st
+import pandas as pd
+import numpy as np 
+import ast
+import plotly.express as px
+from rag_pipeline import main
+
+st.set_page_config(page_title="FloatChat - RAG", layout="wide")
+
+st.title("FloatChat — KVPBot+RAG (ARGO)")
+
+query = st.text_input("Ask a question about ARGO profiles (e.g., 'Show me salinity profiles near the bay of bangal in Febuary 2025')")
+
+if st.button("Run"):
+    if not query.strip():
+        st.error("Enter a question.")
+    else:
+        with st.spinner("Running retrieval + LLM → SQL → DB..."):
+            try:
+                df = main(query)
+                # st.write("DEBUG sample row:", df.iloc[0].to_dict())
+
+            except Exception as e:
+                st.error(f"Error: {e}")
+                st.stop()
+        st.success(f"Returned {len(df)} rows")
+        if df.empty:
+            st.info("No results.")
+        else:
+            st.dataframe(df.head(200))
+            if "LATITUDE" in df.columns and "LONGITUDE" in df.columns:
+                fig_map = px.scatter_geo(
+                    df, lat="LATITUDE", lon="LONGITUDE",
+                    color="PLATFORM_NUMBER",
+                    hover_name="PLATFORM_NUMBER",
+                    title="🌍 Float Locations"
+                )
+                st.plotly_chart(fig_map, use_container_width=True)
+            if {"PRES", "TEMP", "PSAL"}.issubset(df.columns):
+                
+                #temprature - pressure
+                try:
+                    fig_ts = px.scatter(
+                        df,
+                        x="PRES",
+                        y="TEMP",
+                        color="PLATFORM_NUMBER",
+                        title="🌡🧂 Temperature-Pressure (T-P) Diagram",
+                        labels={"PRES": "Pressure (dbar)", "TEMP": "Temperature (°C)"},
+                    )
+                    st.plotly_chart(fig_ts, use_container_width=True)
+                except:
+                    pass
+                col1, col2 = st.columns(2)
+                # 🌡 Temperature vs Depth
+                try:
+                    with col1:
+                        fig_temp = px.line(
+                            df,
+                            x="TEMP", y="PRES",
+                            color="PLATFORM_NUMBER",
+                            title="🌡 Temperature Profiles",
+                            labels={"PRES": "Pressure (dbar)", "TEMP": "Temperature (°C)"}
+                        )
+                        fig_temp.update_yaxes(autorange="reversed")  # depth increases downward
+                        st.plotly_chart(fig_temp, use_container_width=True)
+                except:
+                    pass
+                # 🧂 Salinity vs Depth
+                try:
+                    with col2:
+                        fig_psal = px.line(
+                            df,
+                            x="PSAL", y="PRES",
+                            color="PLATFORM_NUMBER",
+                            title="🧂 Salinity Profiles",
+                            labels={"PRES": "Pressure (dbar)", "PSAL": "Salinity"}
+                        )
+                        fig_psal.update_yaxes(autorange="reversed")
+                        st.plotly_chart(fig_psal, use_container_width=True)
+                
+                except:
+                    pass
+                # 🌡🧂 Temperature vs Salinity (T–S Diagram)
+                try:
+                    fig_ts = px.scatter(
+                        df,
+                        x="PSAL",
+                        y="TEMP",
+                        color="PLATFORM_NUMBER",
+                        title="🌡🧂 Temperature-Salinity (T-S) Diagram",
+                        labels={"PSAL": "Salinity", "TEMP": "Temperature (°C)"},
+                    )
+                    st.plotly_chart(fig_ts, use_container_width=True)
+                except:
+                    pass
+                
+                try:
+                    fig_ts = px.box(
+                        df,
+                        y="TEMP",
+                    )
+                    st.plotly_chart(fig_ts, use_container_width=True)
+                except:
+                    pass
+                
+            else:
+                st.warning("No PRES/TEMP/PSAL columns found in result.")
